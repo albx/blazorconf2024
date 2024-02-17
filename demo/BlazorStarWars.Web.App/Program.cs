66b9ae0d80e2af2a88278f25dc4f8eb730e13bc6
Alberto Mori <albi.mo87@hotmail.it>
2024-02-17 23:42:32 +0100

    Add movie list to home page

--- demo/BlazorStarWars.Web.App/Program.cs
+++ demo/BlazorStarWars.Web.App/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services
+    .AddStarWarsClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://swapi-graphql.netlify.app/.netlify/functions/index"));
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
